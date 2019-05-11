f44b8ec97bd55f94e7e6500ca57261734b8a38d6
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-05-12 00:37:38 +0200

    Ajout de 2 composants avec pbs d'affichage

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,30 @@
   <div id="app">
     <div>
       <img alt="Vue logo" src="./assets/Blockchain_Logo_250_250_001.png">
-      <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <HelloWorld msg="GEO Loc suppliers"/>
     </div>
     <div>
-      <h3>Suppliers Vue.js</h3>
+      <h3>Applications</h3>
       <ul>
         <li><a @click="onSuppliersListClick">Suppliers list</a></li>
         <li><a @click="onMapClick">Suppliers map</a></li>
       </ul>
+      <SuppliersList></SuppliersList>
     </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SuppliersList from './components/SuppliersList.vue'
+import SuppliersMap from './components/SuppliersList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    SuppliersList,
+    SuppliersMap
   },
   methods: {
         onSuppliersListClick: function() {
