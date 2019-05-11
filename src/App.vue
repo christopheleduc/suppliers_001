c6312bcd486edd4d3aa95bebb6173d7af22a9cf9
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-05-11 23:41:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div>
+      <img alt="Vue logo" src="./assets/Blockchain_Logo_250_250_001.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+    <div>
+      <h3>Suppliers Vue.js</h3>
+      <ul>
+        <li><a @click="onSuppliersListClick">Suppliers list</a></li>
+        <li><a @click="onMapClick">Suppliers map</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  methods: {
+        onSuppliersListClick: function() {
+        window.alert('UserClikerList !')
+    },
+        onMapClick: function(){
+        window.alert('UserClikerMap !')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
